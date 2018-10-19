ae1fa91421c9f15f74623f0e15fec67f8f8ed531
Allan Gonçalves <allangoncalves@outlook.com>
2018-10-18 23:44:40 -0300

    Update CreateItemDialog.java

--- app/src/main/java/com/example/allan/unidade2/CreateItemDialog.java
+++ app/src/main/java/com/example/allan/unidade2/CreateItemDialog.java
@@ -1,7 +1,6 @@
 package com.example.allan.unidade2;
 
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.EditText;
 
